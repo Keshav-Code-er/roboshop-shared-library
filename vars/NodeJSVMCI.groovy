97d568c8b48214710a297f5d262560ff07ef28f9
Keshav-Code-er <keshavinsc446@gmail.com>
2025-07-12 00:59:57 +0530

    trigger_pipeline

--- vars/NodeJSVMCI.groovy
+++ vars/NodeJSVMCI.groovy
@@ -80,10 +80,10 @@ def call (Map configMap) {
                               script {
                                     echo 'Deployment'
                                     def params = [
-                                          string(name: 'version' , value: "$packageVersion")
+                                          string(name: 'version' , value: "$packageVersion"),
                                            string(name: 'environment' , value: "dev")
                         ]
-                                    build job: "../${component}-deploy", wait: true, parameters:params
+                                    build job: "../${component}-deploy", wait: true, parameters: params
                               }
                         }
                   }
